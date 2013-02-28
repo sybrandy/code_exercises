dd3f6a0a1047c9cb9890e9c5eebde1f366de6cf4
Casey Sybrandy <sybrandy@gmail.com>
2013-02-28 11:17:40 -0500

    Added the kata for anagrams.

---
+++ anagrams/anagrams.d
@@ -0,0 +1,44 @@
+import std.algorithm;
+import std.exception;
+import std.file;
+import std.stdio;
+import std.string : icmp, toLower;
+
+void main(string[] args)
+{
+    enforce(exists(args[1]));
+
+    bool[string] dict;
+    string[][string] anagrams;
+    foreach (s; File(args[1]).byLine()
+                             .filter!(a => a.length > 1)
+                             .filter!(a => a.indexOf("'") < 0))
+    {
+        if (s.idup.toLower !in dict)
+        {
+            dict[s.idup.toLower] = true;
+            string key = s.dup.sort.idup.toLower;
+            anagrams[key] ~= s.idup;
+        }
+    }
+
+    writeln("Number of words: ", dict.length);
+
+    int longest;
+    int numAnagrams;
+    string[] longestVals;
+    foreach (key, vals; anagrams)
+    {
+        if (vals.length > 1)
+        {
+            if (vals.length > longest)
+            {
+                longest = vals.length;
+                longestVals = vals.dup;
+            }
+            numAnagrams += vals.length;
+        }
+    }
+    writefln("Num Anagrams: %d, Longest: %d", numAnagrams, longest);
+    writeln("Longest values: ", longestVals);
+}
