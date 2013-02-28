993ebbda48b0488fea5aedf45e4152a8f073ea12
Casey Sybrandy <sybrandy@gmail.com>
2013-02-28 09:41:53 -0500

    Small tweaks and added code to test rate of false positives.

--- bloom_filter/bloom.d
+++ bloom_filter/bloom.d
@@ -3,6 +3,7 @@ import std.bitmanip;
 import std.digest.md;
 import std.exception;
 import std.file;
+import std.random;
 import std.stdio;
 
 void main(string[] args)
@@ -21,9 +22,7 @@ void main(string[] args)
         }
         size++;
     }
-    size++;
-    size++;
-    size++;
+    size += 3; // Better ratio of set vs. non-set bits.
     writeln("Size: ", size);
 
     BitArray index;
@@ -44,15 +43,45 @@ void main(string[] args)
         index[idx] = 1;
     }
 
-    size_t numElems;
     size_t setElems;
 
     foreach (e; index)
     {
         if (e) { setElems++; }
-        numElems++;
     }
 
-    writefln("Total: %d, Set: %d, Ratio: %f", numElems, setElems,
-             ((cast(double)setElems / cast(double)numElems) * 100));
+    writefln("Total: %d, Set: %d, Ratio: %f", index.length, setElems,
+             ((cast(double)setElems / cast(double)index.length) * 100));
+
+    // Generate some random words and see how many false positives we get.
+    auto gen = Random(unpredictableSeed);
+    int numHits;
+    int falsePositives;
+    for (int i = 0; i < 100; i++)
+    {
+        char[5] buff;
+        for (int j = 0; j < 5; j++)
+        {
+            buff[j] = cast(char)uniform(97, 123, gen);
+        }
+        digest = md5Of(buff);
+        uint idx = (
+                    (digest[0] << 24) |
+                    (digest[1] << 16) |
+                    (digest[2] << 8) | digest[3]
+                   ) >> (32 - size);
+        idx = (idx % ((2 ^^ size) - 1));
+        if (index[idx] == 1)
+        {
+            writeln("Found word: ", buff);
+            numHits++;
+            auto found = find(File(args[1]).byLine(), buff);
+            if (found.empty)
+            {
+                falsePositives++;
+            }
+        }
+    }
+    writeln("Num hits: ", numHits);
+    writeln("Num false positives: ", falsePositives);
 }
