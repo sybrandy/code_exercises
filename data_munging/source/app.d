ed7adcc5c12297303280a1671a65c2e34fb084c7
Name <email_addr>
2014-03-28 13:26:56 -0400

    Converted to a dub project.
    To facilitate this, combined the two original programs into one, but kept the
    diff code in it's own file.

--- data_munging/source/app.d
+++ data_munging/source/app.d
@@ -5,7 +5,19 @@ import std.stdio : writefln, File;
 import std.string : isNumeric;
 import diff;
 
-void main()
+void football()
+{
+    Record!(string) rec;
+    File("football.dat").byLine()
+            .map!(a => a.split())
+            .filter!(a => a.length == 10)
+            .filter!(a => isNumeric(a[0]))
+            .map!(a => Record!(string)(a[1].idup, parse!int(a[6]), parse!int(a[8])))
+            .findMinDiff(rec);
+    writefln("Team %s, Spread %d", rec.id, rec.diff);
+}
+
+void weather()
 {
     Record!(uint) rec;
     File("weather.dat").byLine()
@@ -15,4 +27,12 @@ void main()
             .map!(a => Record!(uint)(parse!uint(a[0]), parse!uint(a[1]), parse!uint(a[2])))
             .findMinDiff(rec);
     writefln("Day %d, spread %d", rec.id, rec.diff);
+}
+
+void main()
+{
+    writefln("Executing: Football");
+    football;
+    writefln("Executing: Weather");
+    weather;
 }
